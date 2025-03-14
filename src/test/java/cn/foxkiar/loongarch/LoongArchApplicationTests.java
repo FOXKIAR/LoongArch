da742e49f217197204c4f3630e7bfa14b191f027
foxkiar <2399786141@qq.com>
2025-03-14 14:04:08 +0800

    项目初始化

---
+++ src/test/java/cn/foxkiar/loongarch/LoongArchApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.foxkiar.loongarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoongArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
