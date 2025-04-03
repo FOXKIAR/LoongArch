522bbdf817f574748547c009f2dfb02822f69395
foxkiar <2399786141@qq.com>
2025-04-03 15:46:48 +0800

    测试

--- src/test/java/cn/foxkiar/loongarch/LoongArchApplicationTests.java
+++ src/test/java/cn/foxkiar/loongarch/LoongArchApplicationTests.java
@@ -1,5 +1,7 @@
 package cn.foxkiar.loongarch;
 
+import cn.foxkiar.loongarch.util.SizeUtil;
+import cn.hutool.system.oshi.OshiUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,7 +10,9 @@ class LoongArchApplicationTests {
 
     @Test
     void contextLoads() {
-
+        OshiUtil.getDiskStores().forEach(hwDiskStore -> {
+            System.out.println(SizeUtil.formatSize(hwDiskStore.getSize()));
+        });
     }
 
 }
