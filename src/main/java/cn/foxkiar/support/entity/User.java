6d59495b8bb54976c40394cf16bca0e11d8efcd3
FOXKIAR <2399786141@qq.com>
2026-04-13 18:02:29 +0800

    jwt不可在payload中存入密码（已修改）

--- src/main/java/cn/foxkiar/support/entity/User.java
+++ src/main/java/cn/foxkiar/support/entity/User.java
@@ -2,9 +2,6 @@ package cn.foxkiar.support.entity;
 
 import lombok.Data;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Data
 public class User {
     private String account;
@@ -17,12 +14,5 @@ public class User {
             return this.account.equals(((User)obj).account) && this.password.equals(((User)obj).password);
         else
             return false;
-    }
-
-    public Map<String, String> toMap() {
-        Map<String, String> map = new HashMap<>();
-        map.put("account", account);
-        map.put("password", password);
-        return map;
     }
 }
--- src/main/java/cn/foxkiar/support/utils/JwtUtil.java
+++ src/main/java/cn/foxkiar/support/utils/JwtUtil.java
@@ -7,17 +7,19 @@ import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class JwtUtil {
 
     public static String generateToken(User user) {
-        Map<String, String> claims = user.toMap();
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("account", user.getAccount());
         Calendar instance = Calendar.getInstance();
         instance.add(Calendar.HOUR_OF_DAY, 8);
         JWTCreator.Builder builder = JWT.create();
-        claims.forEach(builder::withClaim);
+        builder.withClaim("user", claims);
         return builder.withExpiresAt(instance.getTime())
                 .sign(Algorithm.HMAC256("test"));
     }
